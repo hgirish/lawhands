d8b5845f6430ceb5983449be202c8880190fecc5
hgirish <girish@post.com>
2016-07-09 23:58:40 -0700

    added editortemplates

--- src/lawhands/Models/AccountViewModels/RoleViewModel.cs
+++ src/lawhands/Models/AccountViewModels/RoleViewModel.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
 namespace lawhands.Models.AccountViewModels
 {
     public class RoleViewModel
     {
+        [HiddenInput]
         public string Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
